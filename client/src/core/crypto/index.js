a3d8cf85288080be59ae89d8f6be37698202f7fd
Cimiyagar <mr.cimiyagar@gmail.com>
2023-03-08 19:45:06 +0330

    fixing bugs,,, integrating signal new group encryption into project...

--- client/src/core/crypto/index.js
+++ client/src/core/crypto/index.js
@@ -1,6 +1,6 @@
 
 import { Storage } from "../storage";
-import { createSignalProtocolManager, SignalServerStore } from "./signal/SignalGateway"
+import Signal from './signal';
 
 export default class Crypto {
     static inst;
@@ -10,29 +10,16 @@ export default class Crypto {
     static instance() {
         return Crypto.inst;
     }
-    dummySignalServer;
-    signalProtocolManagerUser;
     async openMesage(senderId, msg) {
-        let decrytedMessage = await this.signalProtocolManagerUser.decryptMessageAsync(senderId, msg);
-        this.dummySignalServer = new SignalServerStore();
-        createSignalProtocolManager(Storage.me.fetchMyUserId(), Storage.me.fetchFirstName(), this.dummySignalServer)
-            .then(signalProtocolManagerUser => {
-                this.signalProtocolManagerUser = signalProtocolManagerUser;
-            });
-        return decrytedMessage;
+        
     }
-    async prepareMessage(receiverId, msg) {
-        let encryptedMessage = await this.signalProtocolManagerUser.encryptMessageAsync(receiverId, msg);
-        return encryptedMessage;
+    async packageMessage(receiverId, msg) {
+        Signal.groupEncrypt()
     }
     constructor() {
         Crypto.inst = this;
         this.openMesage = this.openMesage.bind(this);
-        this.prepareMessage = this.prepareMessage.bind(this);
-        this.dummySignalServer = new SignalServerStore();
-        createSignalProtocolManager(Storage.me.fetchMyUserId(), Storage.me.fetchFirstName(), this.dummySignalServer)
-            .then(signalProtocolManagerUser => {
-                this.signalProtocolManagerUser = signalProtocolManagerUser;
-            });
+        this.packageMessage = this.packageMessage.bind(this);
+
     }
 }
