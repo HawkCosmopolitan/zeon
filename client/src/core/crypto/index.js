8e7e5ada130ad2ca872915705299307984a80eae
Cimiyagar <mr.cimiyagar@gmail.com>
2023-03-14 15:31:25 +0330

    fixing bugs. adding new cyptography modules...

--- client/src/core/crypto/index.js
+++ client/src/core/crypto/index.js
@@ -2,6 +2,7 @@
 import CryptoBrowser from 'crypto-browserify';
 import CryptoJS from 'crypto-js';
 import api from '../api';
+import { Memory } from '../memory';
 
 let rsa = forge.pki.rsa;
 
@@ -15,16 +16,19 @@ export default class Crypto {
     }
     secrets = {};
     async configure() {
-        this.securifyRoom('hall');
-        let { salt, payload } = this.packageMessage('hall', 'hello world !');
-        let myMessage = this.openMesage('hall', { payload, salt });
-        console.log(myMessage);
+        
+    }
+    updateRoomSecret(roomId, roomSecret) {
+        localStorage.setItem(`currentRoomSecret:${roomId}`, roomSecret);
+    }
+    getRoomSecret() {
+        return localStorage.getItem(`currentRoomSecret:${roomId}`);
     }
     generateNewDerivedKey(roomId) {
-        let roomSecret = localStorage.getItem(roomId);
+        let roomSecret = this.getRoomSecret(roomId);
         if (!roomSecret) {
             roomSecret = forge.random.getBytesSync(128);
-            localStorage.setItem(`currentRoomSecret:${roomId}`, roomSecret);
+            this.updateRoomSecret(roomSecret);
         }
         var salt = forge.random.getBytesSync(128);
         var key = forge.pkcs5.pbkdf2(roomSecret, salt, 1000, 16);
@@ -33,19 +37,36 @@ export default class Crypto {
     updateRoomKey(roomId, key, salt) {
         localStorage.setItem(`currentRoomKey:${roomId}`, JSON.stringify({ key, salt }));
     }
+    getRoomKey(roomId) {
+        return JSON.parse(localStorage.getItem(`currentRoomKey:${roomId}`));
+    }
     securifyRoom(roomId) {
         let [key, salt] = this.generateNewDerivedKey(roomId);
         this.updateRoomKey(roomId, key, salt);
+        return key;
     }
     notifyNewRoomKey(roomId, key, salt) {
         this.updateRoomKey(roomId, key, salt);
     }
+    async refreshRoomKey(roomId) {
+        return new Promise(resolve => {
+            let key = this.securifyRoom(roomId);
+            let memberships = Memory.startTrx().temp.memberships.dictPerRoom[roomId];
+            let keyPack = {};
+            Object.values(memberships).forEach(m => {
+                keyPack[me.userId] = this.encryptTextByKeyPair(Memory.startTrx().temp.users.byId[m.userId].publicKey, key);
+            });
+            api.crypto.propagateNewRoomKey(roomId, keyPack, () => {
+                resolve();
+            });
+        });
+    }
     openMesage(roomId, data) {
-        let key = JSON.parse(localStorage.getItem(`currentRoomKey:${roomId}`)).key;
+        let key = this.getRoomKey(roomId).key;
         return CryptoJS.AES.decrypt(data.payload, key).toString(CryptoJS.enc.Utf8);
     }
     packageMessage(roomId, payload) {
-        let { key, salt } = JSON.parse(localStorage.getItem(`currentRoomKey:${roomId}`));
+        let { key, salt } = this.getRoomKey(roomId);
         var encrypted = CryptoJS.AES.encrypt(payload, key);
         return { salt: salt, payload: encrypted };
     }
@@ -84,6 +105,11 @@ export default class Crypto {
     }
     constructor() {
         Crypto.inst = this;
+        this.notifyNewRoomKey = this.notifyNewRoomKey.bind(this);
+        this.getRoomKey = this.getRoomKey.bind(this);
+        this.updateRoomKey = this.updateRoomKey.bind(this);
+        this.getRoomSecret = this.getRoomSecret.bind(this);
+        this.updateRoomSecret = this.updateRoomSecret.bind(this);
         this.securifyRoom = this.securifyRoom.bind(this);
         this.openMesage = this.openMesage.bind(this);
         this.packageMessage = this.packageMessage.bind(this);
